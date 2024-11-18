07c0beee0de0b86698b4fd87398412c41c51c308
youngin0902 <chghoi30@daum.net>
2024-11-19 00:14:04 +0900

    Initial commit

---
+++ src/main/java/com/example/_2024_danpoong_team_39_be/Application.java
@@ -0,0 +1,13 @@
+package com.example._2024_danpoong_team_39_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/_2024_danpoong_team_39_be/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._2024_danpoong_team_39_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
