a4159522696bace8682049cfbd7eae067cfc130b
youngin0902 <chghoi30@daum.net>
2024-11-19 18:14:13 +0900

    feat: 공백 시간 서비스 추천 및 CORS 설정

---
+++ src/main/java/com/example/_2024_danpoong_team_39_be/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example._2024_danpoong_team_39_be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
