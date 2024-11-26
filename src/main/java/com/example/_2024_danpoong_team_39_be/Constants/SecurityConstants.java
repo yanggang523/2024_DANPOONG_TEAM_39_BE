17033b2b8b62ecf54e5fac7b21514716c145e5e0
ganggangme <kga0972@gmail.com>
2024-11-26 20:47:08 +0900

    feat: Spring Security 설정 추가 / 허용할 경로는 SecurityConstants

---
+++ src/main/java/com/example/_2024_danpoong_team_39_be/Constants/SecurityConstants.java
@@ -0,0 +1,4 @@
+package com.example._2024_danpoong_team_39_be.Contents;
+
+public class SecurityConstants {
+}
---
+++ src/main/java/com/example/_2024_danpoong_team_39_be/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example._2024_danpoong_team_39_be.config;
+
+public class SecurityConfig {
+}
