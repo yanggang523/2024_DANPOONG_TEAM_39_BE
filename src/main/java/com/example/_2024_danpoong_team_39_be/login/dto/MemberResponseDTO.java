8e10757ec13c27bb3cf07391605187a8142f8de4
ganggangme <kga0972@gmail.com>
2024-12-03 01:25:51 +0900

    fix: 토큰 반환 오류

--- src/main/java/com/example/_2024_danpoong_team_39_be/login/dto/MemberResponseDTO.java
+++ src/main/java/com/example/_2024_danpoong_team_39_be/login/dto/MemberResponseDTO.java
@@ -20,6 +20,7 @@ public class MemberResponseDTO {
         private Long id;
         private String email;
         private String alias;
+        private String token;
 
         // 기본 생성자 추가
         public JoinResultDTO() {
@@ -27,17 +28,20 @@ public class MemberResponseDTO {
 
         // 전체 매개변수 생성자 (빌더 사용 시 자동 생성)
         @Builder
-        public JoinResultDTO(Long id, String email, String alias) {
+        public JoinResultDTO(Long id, String email, String alias, String token) {
             this.id = id;
             this.email = email;
             this.alias = alias;
+            this.token = token;
+
         }
 
-        public static JoinResultDTO fromMember(Member member) {
+        public static JoinResultDTO fromMember(Member member, String token) {
             return JoinResultDTO.builder()
                     .id(member.getId())
                     .email(member.getEmail())
                     .alias(member.getAlias())
+                    .token(token) // 토큰 포함
                     .build();
         }
     }
